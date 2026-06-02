072a74b346c898f633788409828adf40fa87bc89
annetboona <annetboona@gmail.com>
2026-06-02 08:32:40 +0200

    updating housing features

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <ThemeProvider>
         <AuthProvider>
           <ConfirmProvider>
